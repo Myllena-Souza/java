58e1050c0fc1ec59b255d196aa76b6a61716900f
Myllena-Souza <myllenass.egq@gmail.com>
2023-11-23 20:26:23 -0300

    Ex aulas

--- Aulas/src/exemplo4/Exemplo4.java
+++ Aulas/src/exemplo4/Exemplo4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //VERIFICAÇÃO DE PARIDADE
 
 public class Exemplo4 {
-	public static boolean paridade(int Z){
+	public static boolean paridade(int Z){ //função
 		if (Z %2==0){
 			return true;
 		}
@@ -15,13 +15,18 @@ public class Exemplo4 {
 		
 	public static void main(String[] args) {
 		System.out.println("Digite um número inteiro: ");
-		Scanner scanner = new Scanner (System.in);
+		Scanner scanner = new Scanner (System.in); // class(de gerenciamento de teclado) estancia = construtor 
+		int num = scanner.nextInt(); // pegando o valor do usuario em num
 		
-		
-		
-	}
-		
-
-	}
-
+		if (paridade(num)) {
+			System.out.println("O número digitado é par. ");
+		}
+		else {
+			System.out.println("O número digitado é ímpar. ");
+				
+		}
+	}	
 }
+
+
+
